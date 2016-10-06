992ff710e5250294ffef9010e399c3b5a6892bc7
Brendan Lawlor <brendan.lawlor@mycit.ie>
2016-10-06 12:42:00 +0200

    Corrected script that sets the values of kafka host and port to report correctly.

--- fabric/set-kafka-address.sh
+++ fabric/set-kafka-address.sh
@@ -3,4 +3,4 @@
 kafka1=$(docker-compose -p kafka ps | grep kafka_kafka_1)
 export KAFKA_HOST=$(echo $kafka1 | awk  '{print $4}' | awk -F':' '{print $1}')
 export KAFKA_PORT=$(echo $kafka1 | awk  '{print $4}' | awk -F':' '{print $2}' | awk -F'-' '{print $1}')
-echo Kafka host set to $host and port set to $port
+echo Kafka host set to $KAFKA_HOST and port set to $KAFKA_PORT

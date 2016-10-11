be0e8db53e3dcac128a382b8339250f4af76cafe
Brendan Lawlor <brendan.lawlor@mycit.ie>
2016-10-11 12:02:46 +0200

    Topics were not being created correctly. No longer depending on compose yml to do this. Using explicit create-topics.sh script, called from within a kafka container on the swarm.

---
+++ fabric/create-topics.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source ./set-kafka-address.sh
+docker run -e ZK_HOST=$ZK_HOST -e ZK_PORT=$ZK_PORT wurstmeister/kafka:0.10.0.0 /opt/kafka_2.11-0.10.0.0/bin/kafka-topics.sh --create --zookeeper $ZK_HOST:$ZK_PORT --replication-factor 1 --partitions 24 --topic loader
+docker run -e ZK_HOST=$ZK_HOST -e ZK_PORT=$ZK_PORT wurstmeister/kafka:0.10.0.0 /opt/kafka_2.11-0.10.0.0/bin/kafka-topics.sh --create --zookeeper $ZK_HOST:$ZK_PORT --replication-factor 1 --partitions 24 --topic gccontent
--- fabric/set-kafka-address.sh
+++ fabric/set-kafka-address.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
 kafka1=$(docker-compose -p kafka ps | grep kafka_kafka_1)
+zk1=$(docker-compose -p kafka ps | grep kafka_zookeeper_1)
 export KAFKA_HOST=$(echo $kafka1 | awk  '{print $4}' | awk -F':' '{print $1}')
+export ZK_HOST=$(echo $zk1 | awk  '{print $7}' | awk -F':' '{print $1}')
 export KAFKA_PORT=$(echo $kafka1 | awk  '{print $4}' | awk -F':' '{print $2}' | awk -F'-' '{print $1}')
+export ZK_PORT=$(echo $zk1 | awk  '{print $7}' | awk -F':' '{print $2}' | awk -F'-' '{print $1}')
 echo Kafka host set to $KAFKA_HOST and port set to $KAFKA_PORT
+echo Zookeeper host set to $ZK_HOST and port set to $ZK_PORT

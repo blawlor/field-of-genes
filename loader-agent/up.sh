cd73878a1b773b62e32801c02356616ded0cd23f
Brendan Lawlor <brendan.lawlor@mycit.ie>
2016-10-15 17:41:05 +0200

    Parameterized the up script to be used from main experiment script.

--- loader-agent/up.sh
+++ loader-agent/up.sh
@@ -1,2 +1,4 @@
+size=${1-32} #If not supplied, defaults to 32.
+
 docker-compose -f docker-compose.yml -p fog up -d
-docker-compose -f docker-compose.yml -p fog scale loader=32
+docker-compose -f docker-compose.yml -p fog scale loader=$size

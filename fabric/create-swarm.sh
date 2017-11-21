29ee262ad7ba82e2fdc1c11ec77ca7b1d3c4654e
Brendan Lawlor <brendan.lawlor@mycit.ie>
2017-11-21 12:12:18 +0100

    Swarm now created using latest version of Docker. Based on this document: https://www.digitalocean.com/community/tutorials/how-to-create-a-cluster-of-docker-containers-with-docker-swarm-and-digitalocean-on-ubuntu-16-04

--- fabric/create-swarm.sh
+++ fabric/create-swarm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f ./set-do-token.sh ]; then
-    echo "Please create a set-do-token.sh file which exports a Digital Ocean token."
+    echo "Please create a set-do-token.sh file which exports a DO key value."
     exit 1
 fi
 size=${1-5} #If not supplied, defaults to 5.
@@ -11,10 +11,52 @@ export DIGITALOCEAN_PRIVATE_NETWORKING=true
 export DIGITALOCEAN_SIZE=8gb
 export DIGITALOCEAN_REGION=nyc1
 
+echo "Creating nodes"
+
 docker-machine create -d digitalocean queenbee
 for i in `seq 1 $workerbees`;
 do
-    docker-machine create -d digitalocean workerbee-$i
+    docker-machine create -d digitalocean workerbee$i
 done
-echo "eval $(docker-machine sh queenbee)"
+
+
+
+echo "Setting firewall rules on nodes"
+docker-machine ssh queenbee apt-get install ufw
+docker-machine ssh queenbee ufw allow 22/tcp
+docker-machine ssh queenbee ufw allow 2376/tcp
+docker-machine ssh queenbee ufw allow 2377/tcp
+docker-machine ssh queenbee ufw allow 7946/tcp
+docker-machine ssh queenbee ufw allow 7946/udp
+docker-machine ssh queenbee ufw allow 4789/udp
+docker-machine ssh queenbee ufw --force enable
+docker-machine ssh queenbee systemctl restart docker
+
+for j in `seq 1 $workerbees`;
+do
+docker-machine ssh workerbee$j apt-get install ufw
+docker-machine ssh workerbee$j ufw allow 22/tcp
+docker-machine ssh workerbee$j ufw allow 2376/tcp
+docker-machine ssh workerbee$j ufw allow 7946/tcp
+docker-machine ssh workerbee$j ufw allow 7946/udp
+docker-machine ssh workerbee$j ufw allow 4789/udp
+docker-machine ssh workerbee$j ufw --force enable
+docker-machine ssh workerbee$j systemctl restart docker
+done
+
+echo "Creating Swarm across nodes"
+QUEEN_IP="$(docker-machine ip queenbee)"
+docker-machine ssh queenbee sudo docker swarm init --advertise-addr ${QUEEN_IP}
+SWARM_TOKEN=$(docker-machine ssh queenbee sudo docker swarm join-token -q worker)
+echo "Queen ip is ${QUEEN_IP} and Swarm token is ${SWARM_TOKEN}"
+
+for k in `seq 1 $workerbees`;
+do
+echo "docker-machine ssh workerbee$k sudo docker swarm join --token $SWARM_TOKEN $QUEEN_IP:2377"
+docker-machine ssh workerbee$k sudo docker swarm join --token $SWARM_TOKEN $QUEEN_IP:2377
+done
+
+
+echo "Swarm creation complete. To use the swarm, run the following: "
+echo "eval $(docker-machine env queenbee)"
 
